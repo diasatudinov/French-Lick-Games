b5d466cd08fbe06473deecb5db59019869fafceb
Dias <dias.atudinov@gmail.com>
2025-03-10 17:50:50 +0500

    first commit

---
+++ French Lick Games/French Lick Games/French_Lick_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  French_Lick_GamesApp.swift
+//  French Lick Games
+//
+//  Created by Dias Atudinov on 10.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct French_Lick_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
