d344b149c5f1b702023a9c3d6d481e6c0d77cc0b
Dias <dias.atudinov@gmail.com>
2025-03-11 21:02:20 +0500

    game stop bg add

--- French Lick Games/French Lick Games/FLHelpers/GameStopBoardView.swift
+++ French Lick Games/French Lick Games/FLHelpers/GameStopBoardView.swift
@@ -7,12 +7,92 @@
 
 import SwiftUI
 
+enum GameStopState: CaseIterable {
+    case pause, lose, win
+}
 struct GameStopBoardView: View {
+    @State var gameStopState: GameStopState
+    var firstBtnTapped: () -> ()
+    var secondBtnTapped: () -> ()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(.boardBgFL)
+                .resizable()
+                .scaledToFit()
+            VStack {
+                ZStack {
+                    Image(.textBgFL)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 47)
+                    switch gameStopState {
+                    case .pause:
+                        Text("pause")
+                    case .lose:
+                        Text("You lose!")
+                    case .win:
+                        Text("You win!")
+                    }
+                }.font(.custom(FLFonts.regular.rawValue, size: 24))
+                    .foregroundStyle(.white)
+                    .textCase(.uppercase)
+                
+                if gameStopState == .win {
+                    ZStack {
+                        Image(.coinsBgFL)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 40)
+                        
+                        Text("+ 50")
+                            .font(.system(size: 20, weight: .black))
+                            .foregroundStyle(.milk)
+                            .textCase(.uppercase)
+                            .offset(x: 7)
+                    }
+                }
+                
+                VStack {
+                    Button {
+                        firstBtnTapped()
+                    } label: {
+                        ZStack {
+                            Image(.btnBgFL)
+                                .resizable()
+                                .scaledToFit()
+                            Text(gameStopState == .pause ? "Resume" : "Retry")
+                                .font(.system(size: 20, weight: .black))
+                                .foregroundStyle(.milk)
+                                .textCase(.uppercase)
+                            
+                            
+                        }.frame(height: 52)
+                    }
+                    
+                    Button {
+                        secondBtnTapped()
+                    } label: {
+                        ZStack {
+                            Image(.btnBgFL)
+                                .resizable()
+                                .scaledToFit()
+                            Text("Menu")
+                                .font(.system(size: 20, weight: .black))
+                                .foregroundStyle(.milk)
+                                .textCase(.uppercase)
+                            
+                            
+                        }.frame(height: 52)
+                    }
+                }
+                
+                Spacer()
+            }.padding(.top, 30)
+            
+        }.frame(height: 293)
     }
 }
 
 #Preview {
-    GameStopBoardView()
+    GameStopBoardView(gameStopState: .win, firstBtnTapped: {}, secondBtnTapped: {})
 }
